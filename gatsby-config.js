61c482ae85116196aa280438d68f1772cc56db00
rahul-vinay <135663909+rahul-vinay@users.noreply.github.com>
2024-11-11 15:14:06 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Rahul Vinay',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+      'MS in Computer Science | Passionate about AI',
+    siteUrl: 'https://rahul-vinay.github.io', // No trailing slash allowed!
+    //image: '/og.png', // Path to your image you placed in the 'static' folder
+    ,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
