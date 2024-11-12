2c56fee0797a813a95a803d8339746afe55b9cb7
rahul-vinay <135663909+rahul-vinay@users.noreply.github.com>
2024-11-12 14:16:44 -0600

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,8 +105,8 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+        <a href="https://github.com/rahul-vinay/rahul-portfolio">
+          <div>Adapted from Brittany Chiang's original design</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
