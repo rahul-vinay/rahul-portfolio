9ac5bb10d88398ccc5c461c910b0d27ea3964a79
rahul-vinay <135663909+rahul-vinay@users.noreply.github.com>
2024-11-13 13:25:03 -0600

    Update logo.js

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import logo from '../../images/logo.png'; // Adjusted path based on your folder structure
 
-const IconLoader = () => (
-  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-    <title>Loader Logo</title>
-    <g>
-      <path
-        stroke="currentColor"
-        strokeWidth="5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M 50, 5
-                  L 11, 27
-                  L 11, 72
-                  L 50, 95
-                  L 89, 73
-                  L 89, 28 z"
-      />
-      <foreignObject x="25" y="25" width="50" height="50">
-        <img src={logo} alt="Logo" style={{ width: '100%', height: '100%' }} />
-      </foreignObject>
+const IconLogo = () => (
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+    <title>Logo</title>
+    <g transform="translate(-8.000000, -2.000000)">
+      <g transform="translate(11.000000, 5.000000)">
+        <polygon
+          id="Shape"
+          stroke="currentColor"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          points="39 0 0 22 0 67 39 90 78 68 78 23"
+        />
+        <foreignObject x="12" y="12" width="60" height="60">
+          <img src={logo} alt="Logo" style={{ width: '100%', height: '100%' }} />
+        </foreignObject>
+      </g>
     </g>
   </svg>
 );
 
-export default IconLoader;
+export default IconLogo;
