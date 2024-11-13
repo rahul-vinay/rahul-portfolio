c1d4bdb042d7d6cd600661b049a8fc1b16a5aa80
rahul-vinay <135663909+rahul-vinay@users.noreply.github.com>
2024-11-13 13:38:19 -0600

    Update logo.js

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../images/logo.png'; // Adjusted path based on your folder structure
+import logo from '../../images/logo.png'; // Adjust path if needed
 
 const IconLogo = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
@@ -14,7 +14,8 @@ const IconLogo = () => (
           strokeLinejoin="round"
           points="39 0 0 22 0 67 39 90 78 68 78 23"
         />
-        <foreignObject x="12" y="12" width="60" height="60">
+        {/* Centering the logo by adjusting x and y */}
+        <foreignObject x="12" y="15" width="60" height="60">
           <img src={logo} alt="Logo" style={{ width: '100%', height: '100%' }} />
         </foreignObject>
       </g>
