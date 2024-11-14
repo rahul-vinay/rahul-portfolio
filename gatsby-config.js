19de964e0a641de32827482fa1469d3d7ec7700f
rahul-vinay <135663909+rahul-vinay@users.noreply.github.com>
2024-11-14 14:43:28 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'MS in Computer Science | Passionate about AI',
     siteUrl: 'https://rahul-vinay.github.io', // No trailing slash allowed!
-    //image: '/og.png', // Path to your image you placed in the 'static' folder
+    image: '/og.png', // Path to your image you placed in the 'static' folder
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
